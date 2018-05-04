3359c5c82b88c6b38178ed69817208bdd375069d
Adrien Turiot <didibear77@gmail.com>
2018-05-04 02:23:24 +0200

    Add OAuth management with GitHub

--- app.py
+++ app.py
@@ -1,23 +1,47 @@
-from flask import Flask, redirect
-from flask_graphql import GraphQLView
+import os 
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
+import sys
+from flask import Flask, redirect, url_for, flash, render_template
+from flask_graphql import GraphQLView
 from config import HOSTNAME, PORT, DEBUG
+
 from model import db
 from schema import schema
+from auth import auth_blueprint, login_manager, login_required, logout_user
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'itsatrap'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-db.init_app(app)
-
-app.add_url_rule('/graphql', view_func=GraphQLView.as_view(name="graphql", schema=schema, graphiql=DEBUG, pretty=True))
+app.register_blueprint(auth_blueprint, url_prefix="/login")
+app.add_url_rule('/graphql', view_func=login_required(GraphQLView.as_view(name="graphql", schema=schema, graphiql=DEBUG, pretty=True)))
 
 @app.errorhandler(404)
 def page_not_found(e):
     return redirect("graphql")
 
-if __name__ == '__main__':
-    app.run(HOSTNAME, port=PORT, debug=DEBUG)
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("You have logged out")
+    return redirect(url_for("index"))
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+db.init_app(app)
+login_manager.init_app(app)
+
+if __name__ == "__main__":
+    if "--setup" in sys.argv:
+        with app.app_context():
+            db.create_all()
+            db.session.commit()
+            print("Database tables created")
+    else:
+        app.run(debug=DEBUG)
---
+++ auth.py
@@ -0,0 +1,107 @@
+import sys
+from flask import Flask, redirect, url_for, flash, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm.exc import NoResultFound
+from flask_dance.contrib.github import make_github_blueprint, github
+from flask_dance.consumer.backend.sqla import OAuthConsumerMixin, SQLAlchemyBackend
+from flask_dance.consumer import oauth_authorized, oauth_error
+from flask_login import (
+    LoginManager, UserMixin, current_user,
+    login_required, login_user, logout_user
+)
+from model import db
+
+auth_blueprint = make_github_blueprint(
+    client_id="f75638c39c89011654bc",
+    client_secret="cb9b1536546bc490c8c47f3ea3f779687815ac14",
+)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(256), unique=True)
+    # ... other columns as needed
+
+class OAuth(OAuthConsumerMixin, db.Model):
+    provider_user_id = db.Column(db.String(256), unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    user = db.relationship(User)
+
+# setup login manager
+login_manager = LoginManager()
+login_manager.login_view = 'github.login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# setup SQLAlchemy backend
+auth_blueprint.backend = SQLAlchemyBackend(OAuth, db.session, user=current_user)
+
+# create/login local user on successful OAuth login
+@oauth_authorized.connect_via(auth_blueprint)
+def github_logged_in(blueprint, token):
+    if not token:
+        flash("Failed to log in with GitHub.", category="error")
+        return False
+
+    resp = blueprint.session.get("/user")
+    if not resp.ok:
+        msg = "Failed to fetch user info from GitHub."
+        flash(msg, category="error")
+        return False
+
+    github_info = resp.json()
+    github_user_id = str(github_info["id"])
+
+    # Find this OAuth token in the database, or create it
+    query = OAuth.query.filter_by(
+        provider=blueprint.name,
+        provider_user_id=github_user_id,
+    )
+    try:
+        oauth = query.one()
+    except NoResultFound:
+        oauth = OAuth(
+            provider=blueprint.name,
+            provider_user_id=github_user_id,
+            token=token,
+        )
+
+    if oauth.user:
+        login_user(oauth.user)
+        flash("Successfully signed in with GitHub.")
+
+    else:
+        # Create a new local user account for this user
+        user = User(
+            # Remember that `email` can be None, if the user declines
+            # to publish their email address on GitHub!
+            # email=github_info["email"],
+            username=github_info["name"],
+        )
+        # Associate the new local user account with the OAuth token
+        oauth.user = user
+        # Save and commit our database models
+        db.session.add_all([user, oauth])
+        db.session.commit()
+        # Log in the new local user account
+        login_user(user)
+        flash("Successfully signed in with GitHub.")
+
+    # Disable Flask-Dance's default behavior for saving the OAuth token
+    return False
+
+# notify on OAuth provider error
+@oauth_error.connect_via(auth_blueprint)
+def github_error(blueprint, error, error_description=None, error_uri=None):
+    msg = (
+        "OAuth error from {name}! "
+        "error={error} description={description} uri={uri}"
+    ).format(
+        name=blueprint.name,
+        error=error,
+        description=error_description,
+        uri=error_uri,
+    )
+    flash(msg, category="error")
+
